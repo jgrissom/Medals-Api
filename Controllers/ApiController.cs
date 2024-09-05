f83edf0102e741e3f70227cda5c48a4abac08e28
Jeff Grissom <jgrissom91@gmail.com>
2024-09-04 19:38:28 -0500

    [1.09] Api - http delete

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -24,4 +24,15 @@ public class ApiController(DataContext db) : ControllerBase
         await _dataContext.SaveChangesAsync();
         return country;
     }
+    // http delete member from collection
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(int id){
+        Country? country = await _dataContext.Countries.FindAsync(id);
+        if (country == null){
+            return NotFound();
+        }
+        _dataContext.Remove(country);
+        await _dataContext.SaveChangesAsync();
+        return NoContent();
+    }
 }
