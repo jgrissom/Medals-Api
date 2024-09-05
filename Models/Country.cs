bf5442683fef2b36531cbeb543a427a834c55995
Jeff Grissom <jgrissom91@gmail.com>
2024-09-04 19:29:08 -0500

    [1.04] Data model

---
+++ Models/Country.cs
@@ -0,0 +1,8 @@
+public class Country
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public int Gold { get; set; }
+    public int Silver { get; set; }
+    public int Bronze{ get; set; }
+}
